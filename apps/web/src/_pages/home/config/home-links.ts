7617829ff34d7c7e5bb0244b4b861251641ab78a
Carlos Andrés Castillo Girón <carloscastillo090916@gmail.com>
2026-09-19 10:04:10 -0500

    feat(web): ✨ Render the default Next.js page (#2)
    * feat(web): 🍱 Add the Next.js and Vercel logos
    
    * feat(web): ✨ Render the default Next.js page
    
    * docs(repo): 📝 Stop calling the page blank

---
+++ apps/web/src/_pages/home/config/home-links.ts
@@ -0,0 +1,6 @@
+export const HOME_LINKS = {
+  deploy: "https://vercel.com/new",
+  docs: "https://nextjs.org/docs",
+  learn: "https://nextjs.org/learn",
+  templates: "https://vercel.com/templates?framework=next.js",
+};
